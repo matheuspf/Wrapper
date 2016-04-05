f289b6791195755720a59b1e66f2323251e32abb
matheuspf <matheuspedrozaferreira@uft.edu.br>
2016-04-05 14:46:34 -0300

    Implemented wrapper inheritance

--- Wrapper.h
+++ Wrapper.h
@@ -6,26 +6,13 @@
 namespace wrp
 {
 
-// Defined as a variadic template so it can be specialized in any necessary way
-
-template <typename...>
-struct Wrapper;
-
-
-// Main specialization. Allows for instantiations like 'wrp::Wrapper<Type>', where 'Type' may have any qualifier
-
-template <typename T>
-struct Wrapper<T> : public Wrapper<T, std::decay_t<T>>
-{
-    using Wrapper<T, std::decay_t<T>>::Wrapper;
-};
-
-
-
 // Specialization for fundamental types. The 'std::enable_if' is only necessary here
 
+namespace impl
+{
+
 template <typename T>
-struct Wrapper<T, std::enable_if_t<std::is_fundamental<std::decay_t<T>>::value, std::decay_t<T>>>
+struct Wrapper
 {
     using Type = T;     // Type may have any qualifier
 
@@ -46,6 +33,12 @@ struct Wrapper<T, std::enable_if_t<std::is_fundamental<std::decay_t<T>>::value, 
 
     template <typename U = T>
 	explicit constexpr Wrapper (std::enable_if_t<std::is_reference<U>::value>* = nullptr) : t(impl::lval<BaseType>()) {}
+
+
+
+	template <typename... Args>
+	explicit constexpr Wrapper (Args&&... args) : t(std::forward<Args>(args)...) {}
+
 
 
     template <typename U>
@@ -99,28 +92,52 @@ struct Wrapper<T, std::enable_if_t<std::is_fundamental<std::decay_t<T>>::value, 
 
     decltype(auto) operator << (const BaseType& u) { return (BaseType)t << u; }
 
+};
 
+}
 
+//--------------------------------------------------------------------------------------------------
 //--------------------------------------------------------------------------------------------------
 
 
+// Defined as a variadic template so it can be specialized in any necessary way
 
-#define OP_TYPE(OP, T) Wrapper<decltype(std::declval<BaseType>() OP std::declval<std::decay_t<T>>())>
+template <typename...>
+struct Wrapper;
 
 
-/*#define DEFAULT_OP(OP)  \
-\
-template <typename U>   \
-decltype(auto) operator OP ## = (const U& u) { t OP ## = u; return *this; }    \
-\
-template <typename U>   \
-decltype(auto) operator OP ## = (const Wrapper<U>& w) { t OP ## = w.t; return *this; }     \
-\
-template <typename U>   \
-friend OP_TYPE(OP, U) operator OP (const Wrapper& w, const U& u) { OP_TYPE(OP, U) r(w); r OP ## = u; return r; }    \
-\
-template <typename U>   \
-friend OP_TYPE(OP, U) operator OP (const U& u, const Wrapper& w) { OP_TYPE(OP, U) r(w); r OP ## = u; return r; }*/
+// Main specialization. Allows for instantiations like 'wrp::Wrapper<Type>', where 'Type' may have any qualifier
+
+template <typename T>
+struct Wrapper<T> : public Wrapper<T, std::decay_t<T>>
+{
+    using Wrapper<T, std::decay_t<T>>::Wrapper;
+};
+
+
+template <typename T>
+struct Wrapper<T, std::enable_if_t<std::is_fundamental<std::decay_t<T>>::value, std::decay_t<T>>> : public impl::Wrapper<T>
+{
+
+    using Base = impl::Wrapper<T>;
+
+    using Base::Base;
+    using Base::t;
+
+    using Type     = T;
+    using BaseType = typename Base::BaseType;
+
+
+
+    template <typename U>
+    struct OpType { using Type = Wrapper<decltype(std::declval<BaseType>() * std::declval<std::decay_t<U>>())>; };
+
+    template <typename U>
+    struct OpType<Wrapper<U>> { using Type = typename OpType<U>::Type; };
+
+    template <typename U>
+    using OpType_t = typename OpType<U>::Type;
+
 
 #define DEFAULT_OP(OP)  \
 \
@@ -131,13 +148,77 @@ template <typename U>   \
 decltype(auto) operator OP ## = (const Wrapper<U>& w) { return *this += w.t; }     \
 \
 template <typename U>   \
-friend OP_TYPE(OP, U) operator OP (const Wrapper& w, const Wrapper<U>& z) { OP_TYPE(OP, U) r(w); r OP ## = z.t; return r; }     \
+friend decltype(auto) operator OP (const Wrapper& w, const Wrapper<U>& z) { OpType_t<U> r(w); r OP ## = z.t; return r; }     \
 \
 template <typename U>   \
-friend OP_TYPE(OP, U) operator OP (const Wrapper& w, const U& u) { OP_TYPE(OP, U) r(w); r OP ## = u; return r; }    \
+friend decltype(auto) operator OP (const Wrapper& w, const U& u) { OpType_t<U> r(w); r OP ## = u; return r; }    \
 \
 template <typename U>   \
-friend OP_TYPE(OP, U) operator OP (const U& u, const Wrapper& w) { OP_TYPE(OP, U) r(w); r OP ## = u; return r; }
+friend decltype(auto) operator OP (const U& u, const Wrapper& w) { OpType_t<U> r(w); r OP ## = u; return r; }
+
+
+    DEFAULT_OP(+);
+    DEFAULT_OP(-);
+    DEFAULT_OP(*);
+    DEFAULT_OP(/);
+
+
+#undef OP_TYPE
+#undef DEFAULT_OP
+
+};
+
+
+//========================================================================================
+
+
+
+template <typename T, typename V, int N>
+struct Wrapper<T, cv::Vec<V, N>> : public impl::Wrapper<T>
+{
+    using Base = impl::Wrapper<T>;
+
+    using Base::Base;
+    using Base::t;
+
+    using Type     = T;
+    using BaseType = typename Base::BaseType;
+
+
+
+    template <typename U>
+    struct OpType { using Type = Wrapper<cv::Vec<decltype(std::declval<BaseType>() * std::declval<std::decay_t<U>>()), N>>; };
+
+    template <typename U>
+    struct OpType<Wrapper<U>> { using Type = typename OpType<U>::Type; };
+
+    template <typename U>
+    struct OpType<cv::Vec<U, N>> { using Type = typename OpType<U>::Type; };
+
+    template <typename U>
+    using OpType_t = typename OpType<U>::Type;
+
+
+
+#define DEFAULT_OP(OP)  \
+\
+template <typename U>   \
+decltype(auto) operator OP ## = (const U& u) { for(int i = 0; i < N; ++i) t[i] OP ## = u; return *this; }    \
+\
+template <typename U>   \
+decltype(auto) operator OP ## = (const cv::Vec<U, N>& u) { for(int i = 0; i < N; ++i) t[i] OP ## = u[i]; return *this; }   \
+\
+template <typename U>   \
+decltype(auto) operator OP ## = (const Wrapper<U>& w) { return *this OP ## = w.t; }     \
+\
+template <typename U, typename = std::enable_if_t<!impl::IsWrapper<U>::value>>   \
+friend decltype(auto) operator OP (const Wrapper& w, const U& u) { OpType_t<U> r(w); r OP ## = u; return r; }    \
+\
+template <typename U, typename = std::enable_if_t<!impl::IsWrapper<U>::value>>   \
+friend decltype(auto) operator OP (const U& u, const Wrapper& w) { OpType_t<U> r(w); r OP ## = u; return r; }    \
+\
+template <typename U>   \
+friend decltype(auto) operator OP (const Wrapper& w, const Wrapper<U>& u) { return w OP u.t; }    \
 
 
     DEFAULT_OP(+);
@@ -150,141 +231,11 @@ friend OP_TYPE(OP, U) operator OP (const U& u, const Wrapper& w) { OP_TYPE(OP, U
 #undef DEFAULT_OP
 
 
-    /*template <typename U>
-	decltype(auto) operator *= (const U& u) { t *= u; return *this; }
-
-	template <typename U>
-	decltype(auto) operator *= (const Wrapper<U>& w) { t *= w.t; return *this; }
-
-
-    template <typename U>
-    friend MultType_t<U> operator * (const Wrapper& w, const U& u) { MultType_t<U> r(w); r *= u; return r; }
-
-    template <typename U>
-    friend MultType_t<U> operator * (const U& u, const Wrapper& w) { MultType_t<U> r(w); r *= u; return r; }*/
-
-};
-
-
-
-//=============================================================================================================
-
-
-template <typename T, typename V, int N>
-struct Wrapper<T, cv::Vec<V, N>>
-{
-    using Type = T;     // Type may have any qualifier
-
-    using BaseType = cv::Vec<V, N>;
-    //using BaseType = std::remove_reference_t<T>;    // Here 'BaseType' can have const qualifier
-
-
-
-	Type t;        // The only storage of the class
-
-
-
-    // For non references, the default initialization is called. For references, the default initialization sets
-    // the variable 't' to a empty variable of type 'Type'
-
-    template <typename U = T>
-    explicit constexpr Wrapper (std::enable_if_t<!std::is_reference<U>::value>* = nullptr) : t{} {}
-
-    template <typename U = T>
-	explicit constexpr Wrapper (std::enable_if_t<std::is_reference<U>::value>* = nullptr) : t(impl::lval<BaseType>()) {}
-
-
-    template <typename U>
-	explicit constexpr Wrapper(U& u) : t(u) {}
-
-    template <typename U>
-	explicit constexpr Wrapper(U&& u) : t(std::move(u)) {}
-
-    template <typename U>
-    explicit constexpr Wrapper(const U& u) : t(u) {}
-
-
-    template <typename U>
-    explicit constexpr Wrapper(Wrapper<U>& w) : t(w.t) {}
-
-    template <typename U>
-	explicit constexpr Wrapper(Wrapper<U>&& w) : t(std::move(w.t)) {}
-
-    template <typename U>
-    explicit constexpr Wrapper(const Wrapper<U>& w) : t(w.t) {}
-
-
-
-	template <typename U>
-	explicit constexpr operator U() { return t; }
-
-    template <typename U>
-	explicit constexpr operator U() const { return t; }
-
-
-
-	decltype(auto) operator = (BaseType& x) { t = x; return *this; }
-	decltype(auto) operator = (BaseType&& x) { t = std::move(x); return *this; }
-    decltype(auto) operator = (const BaseType& x) { t = x; return *this; }
-
-    template <typename U>
-    decltype(auto) operator = (Wrapper<U>& w) { t = std::move(w.t); return *this; }
-
-    template <typename U>
-    decltype(auto) operator = (Wrapper<U>&& w) { t = w.t; return *this; }
-
-    template <typename U>
-    decltype(auto) operator = (const Wrapper<U>& w) { t = w.t; return *this; }
-
-
-
-    decltype(auto) operator[] (int p) { return t[p]; }
-
-    decltype(auto) operator[] (int p) const { return t[p]; }
-
-
-    decltype(auto) operator << (const BaseType& u) { return (BaseType)t << u; }
-
-
-
-//=========================================================================================
-
-#define OP_TYPE(OP, T) Wrapper<cv::Vec<decltype(std::declval<V>() OP std::declval<std::decay_t<T>>()), N>>
-
-
-#define DEFAULT_OP(OP)  \
-\
-template <typename U>   \
-decltype(auto) operator OP ## = (const U& u) { for(int i = 0; i < N; ++i) t[i] OP ## = u; return *this; }    \
-\
-\
-template <typename U>   \
-decltype(auto) operator OP ## = (const cv::Vec<U, N>& u) { for(int i = 0; i < N; ++i) t[i] OP ## = u[i]; return *this; }   \
-\
-template <typename U>   \
-decltype(auto) operator OP ## = (const Wrapper<U>& w) { return *this OP ## = w.t; }     \
-\
-template <typename U, typename = std::enable_if_t<!impl::IsWrapper<U>::value>>   \
-friend Wrapper<cv::Vec<double, N>> operator OP (const Wrapper& w, const U& u) { Wrapper<cv::Vec<double, N>> r(w); r OP ## = u; return r; }    \
-\
-template <typename U, typename = std::enable_if_t<!impl::IsWrapper<U>::value>>   \
-friend Wrapper<cv::Vec<double, N>> operator OP (const U& u, const Wrapper& w) { Wrapper<cv::Vec<double, N>> r(w); r OP ## = u; return r; }    \
-\
-template <typename U>   \
-friend decltype(auto) operator OP (const Wrapper& w, const Wrapper<U>& u) { return w OP u.t; }    \
-
-
-    DEFAULT_OP(+);
-    DEFAULT_OP(-);
-    DEFAULT_OP(*);
-
-
 
     template <typename U>
     friend double operator * (const Wrapper& w, const cv::Vec<U, N>& u)
     {
-        //decltype(std::decay_t<V>() * std::decay_t<U>()) r{};
-        double r{};
+        decltype(std::decay_t<V>() * std::decay_t<U>()) r{};
 
         for(int i = 0; i < N; ++i)
             r += w.t[i] * u[i];
@@ -294,10 +245,6 @@ friend decltype(auto) operator OP (const Wrapper& w, const Wrapper<U>& u) { retu
 
     template <typename U>
     friend double operator * (const cv::Vec<U, N>& u, const Wrapper& w) { return w * u; }
-
-
-#undef OP_TYPE
-#undef DEFAULT_OP
 
 };
 
--- main.cpp
+++ main.cpp
@@ -5,12 +5,6 @@ using namespace std;
 /* g++ -I"C:\opencv\build\include" -I"C:\MinGW\include" -L"C:\opencv\mingw64\lib" main.cpp -g C:\MinGW\lib\libboost_thread.a C:\MinGW\lib\libboost_system.a  -lopencv_core249 -lopencv_highgui249 -std=c++14 */
 
 
-template <typename T, class F>
-struct OpType
-{
-    using Type = std::result_of_t<F(T, T)>;
-};
-
 
 int main ()
 {
@@ -19,7 +13,7 @@ int main ()
 
     //wrp::Wrapper<int>(wrp::Wrapper<char>());
 
-    auto x = wrp::Wrapper<cv::Vec<int, 3>>(cv::Vec<int, 3>{1, 2, 3}) * wrp::Wrapper<cv::Vec<double, 3>>(cv::Vec<double, 3>{0.5, 0.1, 2.35});
+    auto x = wrp::Wrapper<cv::Vec<int, 3>>(1, 2, 3) * wrp::Wrapper<cv::Vec<double, 3>>(0.5, 2.4, 3.3);
 
     cout << x << endl;
 
