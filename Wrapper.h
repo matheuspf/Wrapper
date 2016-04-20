49fac0d07142f883263225c5d1f2e4831fd31e4d
matheuspf <matheuspedrozaferreira@uft.edu.br>
2016-04-20 08:26:38 -0300

    Working, but needs a lot of fixes

--- Wrapper.h
+++ Wrapper.h
@@ -4,17 +4,19 @@
 
 #define USING_WRAPPER(...)  using Base = __VA_ARGS__;  \
 \
-                            using Base::Wrapper;    \
+                            using Base::Base;    \
                             using Base::operator=;  \
 \
                             using Base::t;  \
 \
                             using Type     = typename Base::Type;  \
-                            using BaseType = typename Base::BaseType;
+                            using BaseType = typename Base::BaseType;    \
+
 
 
 namespace wrp
 {
+
 
 // Specialization for fundamental types. The 'std::enable_if' is only necessary here
 
@@ -41,31 +43,39 @@ struct Wrapper
     template <typename U = T>
     explicit constexpr Wrapper (std::enable_if_t<!std::is_reference<U>::value>* = nullptr) : t{} {}
 
-    template <typename U = T>
-	explicit constexpr Wrapper (std::enable_if_t<std::is_reference<U>::value>* = nullptr) : t(impl::lval<BaseType>()) {}
+    //template <typename U = T>
+	//explicit constexpr Wrapper (std::enable_if_t<std::is_reference<U>::value>* = nullptr) : t(impl::lval<BaseType>()) {}
 
 
 
-	template <typename... Args>
-	explicit constexpr Wrapper (Args&&... args) : t(std::forward<Args>(args)...) {}
+	//template <typename... Args>
+	//explicit constexpr Wrapper (Args&&... args) : t(std::forward<Args>(args)...) {}
 
 
 
-    template <typename U>
+    template <typename U, typename = std::enable_if_t<!impl::IsWrapper<std::decay_t<U>>::value>>
 	explicit constexpr Wrapper(U& u) : t(u) {}
 
-    template <typename U>
-	explicit constexpr Wrapper(U&& u) : t(std::move(u)) {}
+    //template <typename U, typename = std::enable_if_t<!impl::IsWrapper<std::decay_t<U>>::value>>
+	//explicit constexpr Wrapper(U&& u) : t(std::move(u)) {}
 
-    template <typename U>
+    template <typename U, typename = std::enable_if_t<!impl::IsWrapper<std::decay_t<U>>::value>>
     explicit constexpr Wrapper(const U& u) : t(u) {}
+
+
+	/*constexpr Wrapper(BaseType& u) : t(u) {}
+
+	constexpr Wrapper(BaseType&& u) : t(std::move(u)) {}
+
+    constexpr Wrapper(const BaseType& u) : t(u) {}*/
+
 
 
     template <typename U>
     explicit constexpr Wrapper(Wrapper<U>& w) : t(w.t) {}
 
-    template <typename U>
-	explicit constexpr Wrapper(Wrapper<U>&& w) : t(std::move(w.t)) {}
+    //template <typename U>
+	//explicit constexpr Wrapper(Wrapper<U>&& w) : t(std::move(w.t)) {}
 
     template <typename U>
     explicit constexpr Wrapper(const Wrapper<U>& w) : t(w.t) {}
@@ -79,8 +89,11 @@ struct Wrapper
 	explicit constexpr operator U() const { return t; }
 
 
-    explicit constexpr operator BaseType&() { return t; }
-    explicit constexpr operator BaseType() const { return t; }
+
+    //constexpr operator BaseType&() { return t; }
+
+	//constexpr operator BaseType() const { return t; }
+
 
 
 	decltype(auto) operator = (BaseType& x) { t = x; return *this; }
@@ -96,11 +109,6 @@ struct Wrapper
     template <typename U>
     decltype(auto) operator = (const Wrapper<U>& w) { t = w.t; return *this; }
 
-
-
-    decltype(auto) operator[] (int) { return t; }
-
-    decltype(auto) operator[] (int) const { return t; }
 
 
     decltype(auto) operator << (const BaseType& u) { return (BaseType)t << u; }
@@ -128,10 +136,127 @@ struct Wrapper<T> : public Wrapper<T, std::decay_t<T>>
 };
 
 
-template <typename T>
-struct Wrapper<T, std::enable_if_t<std::is_fundamental<std::decay_t<T>>::value, std::decay_t<T>>> : public impl::Wrapper<T>
+//===================================================================================
+
+//GAMBIRA
+
+template <class T, typename B>
+struct Wrapper<T, Wrapper<B>> //: public std::conditional_t<std::is_reference<T>::value,
+                                                          //Wrapper<std::decay_t<U>, std::decay_t<U>>,
+                                                          //Wrapper<std::decay_t<U>,  std::decay_t<U>>>
 {
-    USING_WRAPPER(impl::Wrapper<T>)
+    /*using BaseWrapper = std::conditional_t<std::is_reference<T>::value,
+                  Wrapper<std::decay_t<U>, std::decay_t<U>>,
+                  Wrapper<std::decay_t<U>,  std::decay_t<U>>>;
+
+    USING_WRAPPER(BaseWrapper);*/
+
+    //=====================================================================
+
+    using Type = T;     // Type may have any qualifier
+
+    using BaseType = std::decay_t<T>;
+    //using BaseType = std::remove_reference_t<T>;    // Here 'BaseType' can have const qualifier
+
+
+
+	Type t;        // The only storage of the class
+
+
+
+    // For non references, the default initialization is called. For references, the default initialization sets
+    // the variable 't' to a empty variable of type 'Type'
+
+    template <typename U = T>
+    explicit constexpr Wrapper (std::enable_if_t<!std::is_reference<U>::value>* = nullptr) : t{} {}
+
+    //template <typename U = T>
+	//explicit constexpr Wrapper (std::enable_if_t<std::is_reference<U>::value>* = nullptr) : t(impl::lval<BaseType>()) {}
+
+
+
+	//template <typename... Args>
+	//explicit constexpr Wrapper (Args&&... args) : t(std::forward<Args>(args)...) {}
+
+
+
+    template <typename U>
+	explicit constexpr Wrapper(U& u) : t(u) {}
+
+    //template <typename U, typename = std::enable_if_t<!impl::IsWrapper<std::decay_t<U>>::value>>
+	//explicit constexpr Wrapper(U&& u) : t(std::move(u)) {}
+
+    template <typename U>
+    explicit constexpr Wrapper(const U& u) : t(u) {}
+
+
+	/*constexpr Wrapper(BaseType& u) : t(u) {}
+
+	constexpr Wrapper(BaseType&& u) : t(std::move(u)) {}
+
+    constexpr Wrapper(const BaseType& u) : t(u) {}*/
+
+
+
+    /*template <typename U>
+    explicit constexpr Wrapper(Wrapper<U>& w) : t(w.t) {}
+
+    template <typename U>
+	explicit constexpr Wrapper(Wrapper<U>&& w) : t(std::move(w.t)) {}
+
+    template <typename U>
+    explicit constexpr Wrapper(const Wrapper<U>& w) : t(w.t) {}*/
+
+
+
+	template <typename U>
+	explicit constexpr operator U() { return t; }
+
+    template <typename U>
+	explicit constexpr operator U() const { return t; }
+
+
+    /*template <typename U>
+	explicit constexpr operator Wrapper<U>() { return Wrapper<U>(t); }
+
+    template <typename U>
+	explicit constexpr operator Wrapper<U>() const { return Wrapper<U>(t); }*/
+
+
+
+    constexpr operator BaseType&() { return t; }
+
+	constexpr operator BaseType() const { return t; }
+
+
+    /*constexpr operator Wrapper<BaseType&>() { return Wrapper<BaseType&>(t); }
+
+	constexpr operator Wrapper<BaseType>() const { return Wrapper<BaseType>(t); }*/
+
+
+
+
+
+	decltype(auto) operator = (BaseType& x) { t = x; return *this; }
+	decltype(auto) operator = (BaseType&& x) { t = std::move(x); return *this; }
+    decltype(auto) operator = (const BaseType& x) { t = x; return *this; }
+
+    /*template <typename U>
+    decltype(auto) operator = (Wrapper<U>& w) { t = std::move(w.t); return *this; }
+
+    template <typename U>
+    decltype(auto) operator = (Wrapper<U>&& w) { t = w.t; return *this; }
+
+    template <typename U>
+    decltype(auto) operator = (const Wrapper<U>& w) { t = w.t; return *this; }*/
+
+
+
+    decltype(auto) operator << (const BaseType& u) { return (BaseType)t << u; }
+
+
+
+//================================================================================
 
 
 
@@ -143,6 +268,178 @@ struct Wrapper<T, std::enable_if_t<std::is_fundamental<std::decay_t<T>>::value, 
 
     template <typename U>
     using OpType_t = typename OpType<U>::Type;
+
+
+
+#define DEFAULT_OP(OP)  \
+\
+template <typename U>   \
+decltype(auto) operator OP ## = (const U& u) { t OP ## = u; return *this; }    \
+\
+template <typename U>   \
+decltype(auto) operator OP ## = (const Wrapper<U>& w) { return *this += w.t; }     \
+\
+template <typename U>   \
+friend decltype(auto) operator OP (const Wrapper& w, const Wrapper<U>& z) { OpType_t<U> r(w); r OP ## = z.t; return r; }     \
+\
+template <typename U, typename = std::enable_if_t<!impl::IsWrapper<U>::value>>   \
+friend decltype(auto) operator OP (const Wrapper& w, const U& u) { OpType_t<U> r(w); r OP ## = u; return r; }    \
+\
+template <typename U, typename = std::enable_if_t<!impl::IsWrapper<U>::value>>   \
+friend decltype(auto) operator OP (const U& u, const Wrapper& w) { OpType_t<U> r(w); r OP ## = u; return r; }
+
+
+    DEFAULT_OP(+);
+    DEFAULT_OP(-);
+    DEFAULT_OP(*);
+    DEFAULT_OP(/);
+
+
+#undef OP_TYPE
+#undef DEFAULT_OP
+
+};
+
+//GAMBIRA
+
+//=================================================================================
+
+
+
+
+template <typename T>
+struct Wrapper<T, std::enable_if_t<std::is_fundamental<std::decay_t<T>>::value, std::decay_t<T>>> //: public impl::Wrapper<T>
+{
+    //USING_WRAPPER(impl::Wrapper<T>)
+
+
+//=====================================================================
+
+    using Type = T;     // Type may have any qualifier
+
+    using BaseType = std::decay_t<T>;
+    //using BaseType = std::remove_reference_t<T>;    // Here 'BaseType' can have const qualifier
+
+
+
+	Type t;        // The only storage of the class
+
+
+
+    // For non references, the default initialization is called. For references, the default initialization sets
+    // the variable 't' to a empty variable of type 'Type'
+
+    template <typename U = T>
+    explicit constexpr Wrapper (std::enable_if_t<!std::is_reference<U>::value>* = nullptr) : t{} {}
+
+    //template <typename U = T>
+	//explicit constexpr Wrapper (std::enable_if_t<std::is_reference<U>::value>* = nullptr) : t(impl::lval<BaseType>()) {}
+
+
+
+	//template <typename... Args>
+	//explicit constexpr Wrapper (Args&&... args) : t(std::forward<Args>(args)...) {}
+
+
+
+    template <typename U, typename = std::enable_if_t<!impl::IsWrapper<std::decay_t<U>>::value>>
+	explicit constexpr Wrapper(U& u) : t(u) {}
+
+    //template <typename U, typename = std::enable_if_t<!impl::IsWrapper<std::decay_t<U>>::value>>
+	//explicit constexpr Wrapper(U&& u) : t(std::move(u)) {}
+
+    template <typename U, typename = std::enable_if_t<!impl::IsWrapper<std::decay_t<U>>::value>>
+    explicit constexpr Wrapper(const U& u) : t(u) {}
+
+
+	/*constexpr Wrapper(BaseType& u) : t(u) {}
+
+	constexpr Wrapper(BaseType&& u) : t(std::move(u)) {}
+
+    constexpr Wrapper(const BaseType& u) : t(u) {}*/
+
+
+
+    template <typename U>
+    explicit constexpr Wrapper(Wrapper<U>& w) : t(w.t) {}
+
+    //template <typename U>
+	//explicit constexpr Wrapper(Wrapper<U>&& w) : t(std::move(w.t)) {}
+
+    template <typename U>
+    explicit constexpr Wrapper(const Wrapper<U>& w) : t(w.t) {}
+
+
+
+	template <typename U>
+	explicit constexpr operator U() { return t; }
+
+    template <typename U>
+	explicit constexpr operator U() const { return t; }
+
+
+    template <typename U>
+	explicit constexpr operator Wrapper<U>() { return Wrapper<U>(t); }
+
+    template <typename U>
+	explicit constexpr operator Wrapper<U>() const { return Wrapper<U>(t); }
+
+
+
+    constexpr operator BaseType&() { return t; }
+
+	constexpr operator BaseType() const { return t; }
+
+
+    constexpr operator Wrapper<BaseType&>() { return Wrapper<BaseType&>(t); }
+
+	constexpr operator Wrapper<BaseType>() const { return Wrapper<BaseType>(t); }
+
+
+
+
+
+	decltype(auto) operator = (BaseType& x) { t = x; return *this; }
+	decltype(auto) operator = (BaseType&& x) { t = std::move(x); return *this; }
+    decltype(auto) operator = (const BaseType& x) { t = x; return *this; }
+
+    template <typename U>
+    decltype(auto) operator = (Wrapper<U>& w) { t = std::move(w.t); return *this; }
+
+    template <typename U>
+    decltype(auto) operator = (Wrapper<U>&& w) { t = w.t; return *this; }
+
+    template <typename U>
+    decltype(auto) operator = (const Wrapper<U>& w) { t = w.t; return *this; }
+
+
+
+    decltype(auto) operator << (const BaseType& u) { return (BaseType)t << u; }
+
+
+    void swap (Wrapper& w)
+    {
+        std::cout << "PQP";
+        BaseType aux(w.t);
+        w.t = t;
+        t = aux;
+    }
+
+
+
+//================================================================================
+
+
+
+    template <typename U>
+    struct OpType { using Type = Wrapper<decltype(std::declval<BaseType>() * std::declval<std::decay_t<U>>())>; };
+
+    template <typename U>
+    struct OpType<Wrapper<U>> { using Type = typename OpType<U>::Type; };
+
+    template <typename U>
+    using OpType_t = typename OpType<U>::Type;
+
 
 
 #define DEFAULT_OP(OP)  \
@@ -180,9 +477,128 @@ friend decltype(auto) operator OP (const U& u, const Wrapper& w) { OpType_t<U> r
 
 
 template <typename T, typename V, int N>
-struct Wrapper<T, cv::Vec<V, N>> : public impl::Wrapper<T>
+struct Wrapper<T, cv::Vec<V, N>> //: public impl::Wrapper<T>
 {
-    USING_WRAPPER(impl::Wrapper<T>)
+    //USING_WRAPPER(impl::Wrapper<T>)
+
+    //=====================================================================
+
+    using Type = T;     // Type may have any qualifier
+
+    using BaseType = std::decay_t<T>;
+    //using BaseType = std::remove_reference_t<T>;    // Here 'BaseType' can have const qualifier
+
+
+
+	Type t;        // The only storage of the class
+
+
+
+    // For non references, the default initialization is called. For references, the default initialization sets
+    // the variable 't' to a empty variable of type 'Type'
+
+    template <typename U = T>
+    explicit constexpr Wrapper (std::enable_if_t<!std::is_reference<U>::value>* = nullptr) : t{} {}
+
+    //template <typename U = T>
+	//explicit constexpr Wrapper (std::enable_if_t<std::is_reference<U>::value>* = nullptr) : t(impl::lval<BaseType>()) {}
+
+
+
+	//template <typename... Args>
+	//explicit constexpr Wrapper (Args&&... args) : t(std::forward<Args>(args)...) {}
+
+
+
+    //template <typename U, typename = std::enable_if_t<!impl::IsWrapper<std::decay_t<U>>::value>>
+	//explicit constexpr Wrapper(U& u) : t(u) {}
+
+    //template <typename U, typename = std::enable_if_t<!impl::IsWrapper<std::decay_t<U>>::value>>
+	//explicit constexpr Wrapper(U&& u) : t(std::move(u)) {}
+
+    //template <typename U, typename = std::enable_if_t<!impl::IsWrapper<std::decay_t<U>>::value>>
+    //explicit constexpr Wrapper(const U& u) : t(u) {}
+
+
+	constexpr Wrapper(BaseType& u) : t(u) {}
+
+	//constexpr Wrapper(BaseType&& u) : t(std::move(u)) {}
+
+    constexpr Wrapper(const BaseType& u) : t(u) {}
+
+
+
+    template <typename U>
+    explicit constexpr Wrapper(Wrapper<U>& w) : t(w.t) {}
+
+    //template <typename U>
+	//explicit constexpr Wrapper(Wrapper<U>&& w) : t(std::move(w.t)) {}
+
+    template <typename U>
+    explicit constexpr Wrapper(const Wrapper<U>& w) : t(w.t) {}
+
+
+
+	template <typename U>
+	explicit constexpr operator U() { return t; }
+
+    template <typename U>
+	explicit constexpr operator U() const { return t; }
+
+
+    template <typename U>
+	explicit constexpr operator Wrapper<U>() { return Wrapper<U>(t); }
+
+    template <typename U>
+	explicit constexpr operator Wrapper<U>() const { return Wrapper<U>(t); }
+
+
+
+    constexpr operator BaseType&() { return t; }
+
+	constexpr operator BaseType() const { return t; }
+
+
+    constexpr operator Wrapper<BaseType&>() { return Wrapper<BaseType&>(t); }
+
+	constexpr operator Wrapper<BaseType>() const { return Wrapper<BaseType>(t); }
+
+
+
+
+
+	decltype(auto) operator = (BaseType& x) { t = x; return *this; }
+	decltype(auto) operator = (BaseType&& x) { t = std::move(x); return *this; }
+    decltype(auto) operator = (const BaseType& x) { t = x; return *this; }
+
+    template <typename U>
+    decltype(auto) operator = (Wrapper<U>& w) { t = w.t; return *this; }
+
+    //template <typename U>
+    //decltype(auto) operator = (Wrapper<U>&& w) { t = std::move(w.t); return *this; }
+
+    template <typename U>
+    decltype(auto) operator = (const Wrapper<U>& w) { t = w.t; return *this; }
+
+
+
+    decltype(auto) operator << (const BaseType& u) { return (BaseType)t << u; }
+
+
+
+
+    decltype(auto) operator [] (int p) { return t[p]; }
+
+    const decltype(auto) operator [] (int p) const { return t[p]; }
+
+
+//================================================================================
+
+
+
+
+
+
 
 
     template <typename U>
@@ -201,7 +617,7 @@ struct Wrapper<T, cv::Vec<V, N>> : public impl::Wrapper<T>
 
 #define DEFAULT_OP(OP)  \
 \
-template <typename U>   \
+template <typename U, typename = std::enable_if_t<!impl::IsWrapper<std::decay_t<U>>::value && !impl::IsVec<std::decay_t<U>>::value>>   \
 decltype(auto) operator OP ## = (const U& u) { for(int i = 0; i < N; ++i) t[i] OP ## = u; return *this; }    \
 \
 template <typename U>   \
@@ -210,10 +626,10 @@ decltype(auto) operator OP ## = (const cv::Vec<U, N>& u) { for(int i = 0; i < N;
 template <typename U>   \
 decltype(auto) operator OP ## = (const Wrapper<U>& w) { return *this OP ## = w.t; }     \
 \
-template <typename U, typename = std::enable_if_t<!impl::IsWrapper<U>::value>>   \
+template <typename U, typename = std::enable_if_t<!impl::IsWrapper<std::decay_t<U>>::value>>   \
 friend decltype(auto) operator OP (const Wrapper& w, const U& u) { OpType_t<U> r(w); r OP ## = u; return r; }    \
 \
-template <typename U, typename = std::enable_if_t<!impl::IsWrapper<U>::value>>   \
+template <typename U, typename = std::enable_if_t<!impl::IsWrapper<std::decay_t<U>>::value>>   \
 friend decltype(auto) operator OP (const U& u, const Wrapper& w) { OpType_t<U> r(w); r OP ## = u; return r; }    \
 \
 template <typename U>   \
--- main.cpp
+++ main.cpp
@@ -1,28 +1,40 @@
 #include "Wrapper.h"
 
-using namespace std;
+
 
 /* g++ -I"C:\opencv\build\include" -I"C:\MinGW\include" -L"C:\opencv\mingw64\lib" main.cpp -g C:\MinGW\lib\libboost_thread.a C:\MinGW\lib\libboost_system.a  -lopencv_core249 -lopencv_highgui249 -std=c++14 */
 
 
 
+using namespace std;
+
+
+
+
 int main ()
 {
-    wrp::Wrapper<cv::Vec<char, 3>>() + wrp::Wrapper<cv::Vec<double, 3>>();
+    vector<uchar> v = {3, 5, 1, 4, 2};
+
+    vector<wrp::Wrapper<uchar&>> u;
+    vector<wrp::Wrapper<uchar>> w;
+
+    for(int i = 0; i < v.size(); ++i)
+    {
+        u.emplace_back(v[i]);
+        w.emplace_back(v[i]);
+    }
 
 
-    //auto x = wrp::Wrapper<cv::Vec<int, 3>>(1, 2, 3) * wrp::Wrapper<cv::Vec<double, 3>>(0.5, 2.4, 3.3);
-
-    //cout << x << endl;
-
-
-    //wrp::impl::PrintType<decltype(x)>();
+    std::sort(u.begin(), u.end(), [](const auto& x, const auto& y){ return x.t < y.t; });
+    std::sort(w.begin(), w.end(), [](const auto& x, const auto& y){ return x.t < y.t; });
 
 
-    //cout << x << endl;
+    //for(int i = 0; i < u.size(); ++i) u[i] += 10;
 
+    for(int i = 0; i < u.size(); ++i) cout << int(u[i]) << endl;
+    cout << endl;
 
-    //auto x = w[20];
+    for(int i = 0; i < w.size(); ++i) cout << int(w[i]) << endl;
 
     return 0;
 }
